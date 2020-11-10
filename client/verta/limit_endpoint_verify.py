b7dec7351998f564203f637d85fea963c7f53b36
Nhat Pham <nphamcs@gmail.com>
2020-11-10 13:39:48 -0800

    Use random name.

--- client/verta/limit_endpoint_verify.py
+++ client/verta/limit_endpoint_verify.py
@@ -1,10 +1,11 @@
 from verta import Client
+from verta._internal_utils._utils import generate_default_name
 
 client = Client(host="trial.dev.verta.ai")
 
-endpoint = client.set_endpoint("/first-endpoint")
+endpoint = client.set_endpoint(generate_default_name())
 try:
-    endpoint2 = client.create_endpoint("/second-endpoint")
+    endpoint2 = client.create_endpoint(generate_default_name())
 except Exception as err:
     print(err)
 finally:
